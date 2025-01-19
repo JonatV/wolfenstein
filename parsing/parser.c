ea958b2a05eb3153903fa82a7036ac20f54e60e6
Edoulazone <eduardoschmitz.p@gmail.com>
2025-01-19 18:51:05 +0100

    wall check

--- parsing/parser.c
+++ parsing/parser.c
@@ -6,11 +6,39 @@
 /*   By: eschmitz <eschmitz@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 12:05:49 by eschmitz          #+#    #+#             */
-/*   Updated: 2025/01/16 18:23:25 by eschmitz         ###   ########.fr       */
+/*   Updated: 2025/01/19 18:10:06 by eschmitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolfenstein.h"
+
+void	map_pars(t_game *data, char *file)
+{
+	int		check;
+	int		fd;
+	char	*str;
+	
+	check = 1;
+	str = NULL;
+	fd = open(file, O_RDONLY);
+
+	if (!(data->map = malloc(sizeof(char *) * data->nbrlines)))
+		return ;
+	while (check)
+	{
+		check = get_next_line(fd, &str, data);
+		if (data->in_map == 1 && !empty_line(str) && data->count < data->nbrlines)
+			data->empty_line = 1;
+		if ((data->in_map = is_map(data, str)) == 1)
+		{
+			data->count++;
+			create_map(data, str);
+		}
+		free(str);
+	}
+	close(fd);
+	init_sprite(data);
+}
 
 void	parse_all(t_game *data, char *file)
 {
