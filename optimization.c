e137baa4aaf102671fec68d3477a656b2d9f0013
JonatV <mrangelsjon@gmail.com>
2025-01-19 23:40:31 +0100

    refactor: change some data (not impactful)

--- optimization.c
+++ optimization.c
@@ -6,7 +6,7 @@ void	img_pix_put(t_img *img, int x, int y, int color)
 		
 		if (x < 0 || y < 0 || x >= img->width || y >= img->height)
 			return ;
-		if (color == (int)0xFF000000)
+		if (color == (int)0xFF000000) //todo: check if mandatory: it seems like if it is activated, it will be black instead of transparent
 			return ;
 		pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
 		*(int *)pixel = color;
